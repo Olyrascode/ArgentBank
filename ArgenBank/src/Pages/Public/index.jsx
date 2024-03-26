a8b2788ed06ba3cbe474aa5bae186344899f9c8c
Olyrascode <120477523+Olyrascode@users.noreply.github.com>
2024-03-26 15:19:17 +0100

    Delete ArgenBank directory

--- ArgenBank/src/Pages/Public/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Error } from './Error/Error.jsx'
-export { default as Home } from './Home/Home.jsx'
-export { default as SignIn } from './SignIn/SignIn.jsx'
-export { default as User } from '../Auth/User/User.jsx'
